92884dcc916ac738c9fececdc631eeefde5d8c8f
wangyi <haveaballmyball@hotmail.com>
2021-03-14 19:43:47 +0800

    chore:修改配置

--- src/modules.js
+++ src/modules.js
@@ -1,11 +1,19 @@
 console.log(process.env.NODE_ENV);
 const LOCAL_IP_ADDRESS = "localhost";
-const SERVE_IP_ADDRESS = "106.54.42.57";
+const SERVE_IP_ADDRESS = "matserw.com";
 export const modules = {
-  "module-html": `http://${
-    process.env.NODE_ENV === "development" ? LOCAL_IP_ADDRESS : SERVE_IP_ADDRESS
-  }:9001/module-html/main.js`,
-  "module-css": `http://${
-    process.env.NODE_ENV === "development" ? LOCAL_IP_ADDRESS : SERVE_IP_ADDRESS
-  }:9002/module-css/main.js`
+  // "module-html": `http://${
+  //   process.env.NODE_ENV === "development" ? LOCAL_IP_ADDRESS : SERVE_IP_ADDRESS
+  // }:9001/module-html/main.js`,
+  // "module-css": `http://${
+  //   process.env.NODE_ENV === "development" ? LOCAL_IP_ADDRESS : SERVE_IP_ADDRESS
+  // }:9002/module-css/main.js`
+  "module-html":
+    process.env.NODE_ENV === "development"
+      ? `http://${LOCAL_IP_ADDRESS}:9001/module-html/main.js`
+      : `https://${SERVE_IP_ADDRESS}:9001/module-html/main.js`,
+  "module-css":
+    process.env.NODE_ENV === "development"
+      ? `http://${LOCAL_IP_ADDRESS}:9001/module-css/main.js`
+      : `https://${SERVE_IP_ADDRESS}:9001/module-css/main.js`,
 };
