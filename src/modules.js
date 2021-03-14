0d6be1acf245b566d5554318794abc9fc45cd4f1
wangyi <haveaballmyball@hotmail.com>
2021-03-14 21:27:31 +0800

    chore:修改配置

--- src/modules.js
+++ src/modules.js
@@ -1,19 +1,19 @@
 console.log(process.env.NODE_ENV);
 const LOCAL_IP_ADDRESS = "localhost";
-const SERVE_IP_ADDRESS = "matserw.com";
+const SERVE_IP_ADDRESS = "blog.matserw.com";
 export const modules = {
-  // "module-html": `http://${
-  //   process.env.NODE_ENV === "development" ? LOCAL_IP_ADDRESS : SERVE_IP_ADDRESS
-  // }:9001/module-html/main.js`,
-  // "module-css": `http://${
-  //   process.env.NODE_ENV === "development" ? LOCAL_IP_ADDRESS : SERVE_IP_ADDRESS
-  // }:9002/module-css/main.js`
-  "module-html":
-    process.env.NODE_ENV === "development"
-      ? `http://${LOCAL_IP_ADDRESS}:9001/module-html/main.js`
-      : `https://${SERVE_IP_ADDRESS}:9001/module-html/main.js`,
-  "module-css":
-    process.env.NODE_ENV === "development"
-      ? `http://${LOCAL_IP_ADDRESS}:9001/module-css/main.js`
-      : `https://${SERVE_IP_ADDRESS}:9001/module-css/main.js`,
+  "module-html": `http://${
+    process.env.NODE_ENV === "development" ? LOCAL_IP_ADDRESS : SERVE_IP_ADDRESS
+  }:9001/module-html/main.js`,
+  "module-css": `http://${
+    process.env.NODE_ENV === "development" ? LOCAL_IP_ADDRESS : SERVE_IP_ADDRESS
+  }:9002/module-css/main.js`
+  // "module-html":
+  //   process.env.NODE_ENV === "development"
+  //     ? `http://${LOCAL_IP_ADDRESS}:9001/module-html/main.js`
+  //     : `https://${SERVE_IP_ADDRESS}:9001/module-html/main.js`,
+  // "module-css":
+  //   process.env.NODE_ENV === "development"
+  //     ? `http://${LOCAL_IP_ADDRESS}:9001/module-css/main.js`
+  //     : `https://${SERVE_IP_ADDRESS}:9001/module-css/main.js`,
 };
