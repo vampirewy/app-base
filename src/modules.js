ebdc0f43cc2719e94b6dc8438c193fac67774b8c
wangyi <haveaballmyball@hotmail.com>
2021-03-15 10:06:53 +0800

    chore:修改配置

--- src/modules.js
+++ src/modules.js
@@ -4,10 +4,10 @@ const SERVE_IP_ADDRESS = "blog.matserw.com";
 export const modules = {
   "module-html": `http://${
     process.env.NODE_ENV === "development" ? LOCAL_IP_ADDRESS : SERVE_IP_ADDRESS
-  }:9001/module-html/main.js`,
+  }/module-html/main.js`,
   "module-css": `http://${
     process.env.NODE_ENV === "development" ? LOCAL_IP_ADDRESS : SERVE_IP_ADDRESS
-  }:9002/module-css/main.js`
+  }/module-css/main.js`
   // "module-html":
   //   process.env.NODE_ENV === "development"
   //     ? `http://${LOCAL_IP_ADDRESS}:9001/module-html/main.js`
